e315b8967d8edcdce7a12c3a61117d7d137126c3
jojo <elokajosephine10@gmail.com>
2026-06-03 17:24:57 -0700

    Add s3 encryption for terraform state bucket

--- bootstrap.tf
+++ bootstrap.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "cloud-mastery-tfstate-bucket"
+  bucket = "cloud-mastery-tfstate-bucket-677920913262"
 
   tags = {
     Name = "Terraform State Bucket"
@@ -25,4 +25,14 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
